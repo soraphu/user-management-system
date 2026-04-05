1bb87c92152e13bafc25827c75e4ffda3b768a0d
soraphu <soraphu@gmail.com>
2026-04-05 11:38:45 +0700

    feat: Add end points.

--- server/server.php
+++ server/server.php
@@ -1,36 +1,62 @@
 <?php
 // Get the path (e.g., /api/users or /shop/products)
 $request = $_SERVER['REQUEST_URI'];
+$request_method = $_SERVER['REQUEST_METHOD'];
 
 // Clean it up (remove query strings like ?id=1)
 $path = parse_url($request, PHP_URL_PATH);
 
+header('Content-Type: application/json');
+
 // Now do "whatever you want" based on the path
 switch ($path) {
-    case '/':
-        header('Content-Type: application/json');
-        echo json_encode([
-            "message" => "Welcome to the PHP server!",
-            "endpoints" => [
-                "/api/status - Check server status",
-                "/api/save-sensor - Save sensor data from ESP32"
-            ]
-        ]);
-        break;
+    case '/api':
+        if ($request_method == "GET") {
+            echo json_encode([
+                "message" => "Welcome to the PHP server!",
+                "endpoints" => [
+                    "/api/user-login/:id - Get login user id",
+                    "/api/user-register - Sign up user",
+                    "/api/user-reset-password - Reset user password"
+                ]
+            ]);
+        } else {
+            faultMethod();
+        }
+        break; //End of case
 
-    case '/api/status':
-        header('Content-Type: application/json');
-        echo json_encode(["status" => "running"]);
-        break;
+    case '/api/user-login':
+        if ($request_method == "GET") {
+            echo json_encode(["status" => "Get login running"]);
+        } else {
+            faultMethod();
+        }
+        break; //End of case
 
-    case '/api/save-sensor':
-        // Logic for your ESP32 project
-        header('Content-Type: application/json');
-        echo "Data saved!";
-        break;
+    case '/api/user-register':
+        if ($request_method == "POST") {
+            echo json_encode(["status" => "Post register running"]);
+        } else {
+            faultMethod();
+        }
+        break; //End of case
+
+    case '/api/user-reset-password':
+        if ($request_method == "POST") {
+            echo json_encode(["status" => "Post reset password running"]);
+        } else {
+            faultMethod();
+        }
+        break; //End of case
 
     default:
         http_response_code(404);
         echo "404 - Page not found";
         break;
 }
+
+function faultMethod()
+{
+    http_response_code(405);
+    echo json_encode(["error" => "Method not allowed"]);
+}//End of function
