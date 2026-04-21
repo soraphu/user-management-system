3a4640ea866f580a958ad7ee5ca11d99a51f5e58
soraphu <soraphu@gmail.com>
2026-04-21 20:32:26 +0700

    feat: Prepare for first page.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,12 @@
-function App() {
+import { useState, type JSX } from 'react'
+
+function App(): JSX.Element {
+  const [name, setName] = useState('Soraphu');
 
   return (
     <>
-      <h1>Welcome to My App</h1>
+      <h1>Welcome to My App {name} </h1>
+      <button onClick={() => setName('Alice')}>Change Name</button>
     </>
   )
 }
