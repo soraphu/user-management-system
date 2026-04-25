a793751797c28306aeb1fb86374b688e972d6a4d
soraphu <soraphu@gmail.com>
2026-04-25 21:21:35 +0700

    feat: Design login page.

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,9 +1,31 @@
+import { FaGithub } from "react-icons/fa";
 import LoginForm from "../components/LoginForm"
 
 const LoginPage = () => {
     return (
-        <div>
-            <LoginForm />
+        <div className="grid min-h-svh lg:grid-cols-2">
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex justify-center gap-2 md:justify-start">
+                    <a href="https://github.com/soraphu" target="_blank" className="flex items-center gap-2 font-medium">
+                        <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                            <FaGithub size={18} className="hover:text-gray-400 transition-colors" />
+                        </div>
+                        Soraphu Github
+                    </a>
+                </div>
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        <LoginForm />
+                    </div>
+                </div>
+            </div>
+            <div className="relative hidden bg-muted lg:block">
+                <img
+                    src="/placeholder.svg"
+                    alt="Image"
+                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                />
+            </div>
         </div>
     )
 }
