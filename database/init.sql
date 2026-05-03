09c18bd05f251712905c3a8a8ffb5c324a122a2b
soraphu <soraphu@gmail.com>
2026-05-03 23:32:46 +0700

    fix: user_id must UNIQUE because i will insert instead of update on duplicate.

--- database/init.sql
+++ database/init.sql
@@ -52,6 +52,7 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     expires_at DATETIME NOT NULL,
     
     UNIQUE (token),
+    UNIQUE (user_id),
     FOREIGN KEY (user_id) 
         REFERENCES accounts(id) 
         ON DELETE CASCADE 
