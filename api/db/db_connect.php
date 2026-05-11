12c588ff6a191f070eec8e0b8c1cc8c9e0b150ac
soraphu <soraphu@gmail.com>
2026-05-11 15:30:08 +0700

    fix: Customize for deploy on Render. - Use Aiven database instead of local MySql database ( for deploy ).

--- api/db/db_connect.php
+++ api/db/db_connect.php
@@ -5,15 +5,19 @@ $host = $_ENV['MYSQL_HOST'];         // The service name of your MySQL container
 $database = $_ENV['MYSQL_DB_NAME'];   // The name you gave to MYSQL_DATABASE 
 $username = $_ENV['MYSQL_USERNAME'];   // The name you gave to MYSQL_USER 
 $pass = $_ENV['MYSQL_PASS'];  // The name you gave to MYSQL_PASSWORD 
+$port = $_ENV['MYSQL_PORT'];
+$cert = __DIR__ . '/certs/ca.pem';
 $charset = 'utf8mb4';
 
 // 2. The Data Source Name (DSN)
-$dsn = "mysql:host=$host;dbname=$database;charset=$charset;port=3306";
+$dsn = "mysql:host=$host;dbname=$database;charset=$charset;port=$port";
 // 3. Connection Options
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Halts script on error
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Returns data as associative arrays
-    PDO::ATTR_EMULATE_PREPARES => false,                  // Uses real prepared statements for security
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    // THE SSL CONFIGURATION
+    PDO::MYSQL_ATTR_SSL_CA => $cert,
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,                 // Uses real prepared statements for security
 ];
 
 try {
